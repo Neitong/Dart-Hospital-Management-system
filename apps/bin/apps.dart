ba0d89209351ff8846e8f69f64e5ef26f977c9f6
Nei tong <Hengneitong.keo@student.cadt.edu.kh>
2025-10-19 04:15:12 +0700

    Testing

--- apps/bin/apps.dart
+++ apps/bin/apps.dart
@@ -1,6 +1,10 @@
+import 'dart:js_interop';
+
 import 'package:apps/apps.dart' as apps;
 
 void main(List<String> arguments) {
-
   print('Connected...');
+  // The following lines were redundant or incorrect and have been removed.
+  // print.call('Connected...'); is the same as print('Connected...');
+  // print.jsify(); causes a compilation error.
 }
